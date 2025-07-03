fb3d47d84cb231cfa8b5406d69646dbe6a0e19c7
very-many <99022483+very-many@users.noreply.github.com>
2025-07-04 00:41:16 +0200

    Completed assignment

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import greetingHandler from "./lib/greeter.js";
+import loginHandler from "./lib/loginUeberpruefen.js";
+import booleanHandler from "./lib/booleanAuswertung.js";
+import komplexerVergleichHandler from "./lib/komplexerVergleich.js";
+import pokemonRouteHandler from "./lib/pokemonRoute.js";
 
 const server = express();
 
+ /* Initial */
 server.get("/", greetingHandler);
+
+/* Aufgaben */
+server.get("/login", loginHandler);
+server.get("/boolean", booleanHandler);
+server.get("/komplexer-vergleich", komplexerVergleichHandler);
+
+/* Bonus Aufgaben */
+server.get("/pokemon", pokemonRouteHandler);
+
 
 // Hier die Handler für die Übungsaufgaben in unterschiedlichen
 // Routen definieren. Bspw.:
