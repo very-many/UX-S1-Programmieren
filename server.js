cf70f984f9178f68bd8437c5028c8bb75e3cc0c9
Andy Junginger <andy.junginger@gmail.com>
2025-07-03 16:17:46 +0200

    [TASK] add example handler

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 import express from "express";
+import greetingHandler from "./lib/greeter.js";
 
 const server = express();
 
-server.get("/", (req, res) => {
-    const nameParam = req.query.name || "Jane Doe";
-    res.send(`Hallo ${nameParam}`);
-});
+server.get("/", greetingHandler);
 
 // Hier die Handler für die Übungsaufgaben in unterschiedlichen
 // Routen definieren. Bspw.:
 //
-// server.get("/aufgabe-1", (req, res) => {...
-// server.get("/aufgabe-2", (req, res) => {...
-// server.get("/aufgabe-3", (req, res) => {...
+// server.get("/aufgabe-1", handlerOne);
+// server.get("/aufgabe-2", handlerTwo);
+// server.get("/aufgabe-3", handlerThree);
 
 server.listen(8000, () => {
     console.log("Server is running...");
